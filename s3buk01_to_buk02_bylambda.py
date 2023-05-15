1a10a8e49c8dcd09ba884df2ac682ab029eb82fe
Mave li <lidanmei@yahoo.com>
2023-05-15 12:09:56 +1200

    add code

--- s3buk01_to_buk02_bylambda.py
+++ s3buk01_to_buk02_bylambda.py
@@ -5,19 +5,20 @@ from datetime import datetime
 import os
 from pytz import timezone
 import dotenv 
-
-
-
 def lambda_handler(event,context):
-    s3_client = boto3.client('s3')
-    bucket_name = event['Records'][0]['s3']['bucket']['name']
-    key = event['Records'][0]['s3']['object']['key']
-    key = urllib.parse.unquote_plus(key,encoding='utf-8')
-    message = 'hey this file got uploaded' + key + 'to this bucket' + bucket_name
-    print(message)
-    response = s3_client.get_object(Bucket=bucket_name,Key = key)
-    contents = response["Body"].read().decode()
-    contents =json.loads(contents)
-    for content in contents:
-        print(content['_id'])
-        print(content['title'])
+   
+    # bucket_name = event['Records'][0]['s3']['bucket']['name']
+    # key = event['Records'][0]['s3']['object']['key']
+    # key = urllib.parse.unquote_plus(key,encoding='utf-8')
+    # message = 'hey this file got uploaded' + key + 'to this bucket' + bucket_name
+    # print(message)
+    # response = s3_client.get_object(Bucket=bucket_name,Key = key)
+    # contents = response["Body"].read().decode()
+    # contents =json.loads(contents)
+    # for content in contents:
+    #     print(content['_id'])
+    #     print(content['title'])
+   s3_client = boto3.client('s3')
+   response = s3_client.get_object(Bucket='jobbuck',Key='jobsTest02.json')
+   data=response['Body'].read()
+   print(data)
