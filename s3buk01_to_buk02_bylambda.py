c5c13c88a85283ec20ffb2bd0f46d46dcaf4a2f0
Mave li <lidanmei@yahoo.com>
2023-05-15 23:40:28 +1200

    read and write to targetjobbuck

--- s3buk01_to_buk02_bylambda.py
+++ s3buk01_to_buk02_bylambda.py
@@ -6,9 +6,23 @@ import os
 def lambda_handler(event,context):
    s3_client = boto3.client('s3')
    response = s3_client.get_object(Bucket='jobbuck',Key='jobsTest02.json')
-   data=response['Body'].read()
-   contents=json.loads()
-   print(data)
+   contents = response['Body'].read()
+   contents=json.loads(contents)
+
+   try:
+       for content in contents:
+         if not content:
+            return None
+            print(content)
+         else:
+              data= json.dumps(content).encode('UTF-8')
+              
+              s3_client.put_object(Bucket = 'targetjobbuck',Key='jobsTest03.json',Body = data )
+   except Exception as err:     
+      print(err)
+
+
+   # print(data)
    # bucket_name = event['Records'][0]['s3']['bucket']['name']
    #  key = event['Records'][0]['s3']['object']['key']
    #  key = urllib.parse.unquote_plus(key,encoding='utf-8')
